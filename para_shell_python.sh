35f812db7a1cf03a6358e3502a22d44e7e0ea440
root <root@lab.arvind.com>
2019-01-04 22:04:00 +0530

    Modified parmaterized shell script to download python script

--- para_shell_python.sh
+++ para_shell_python.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/ae07177/arvind_sourcec/master/para_shell_python.sh
+wget https://raw.githubusercontent.com/ae07177/arvind_sourcec/master/test_params.py
 python test_params.py $name
